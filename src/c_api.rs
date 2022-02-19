deb020c24a48702293d31da870f2d6d4b9719d61
David Shlemayev <david.shlemayev@gmail.com>
2022-02-19 15:12:48 +0200

    Expose oplist in python api

--- src/c_api.rs
+++ src/c_api.rs
@@ -7,7 +7,7 @@ use std::ffi::CStr;
 use std::os::raw::c_char;
 use std::panic::catch_unwind;
 use std::ptr::null;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 #[no_mangle]
 pub static SIMPLEXP_VERSION_MAJOR: u32 = unwrap_ctx!(parse_u32(env!("CARGO_PKG_VERSION_MAJOR")));
@@ -18,20 +18,20 @@ pub static SIMPLEXP_VERSION_PATCH: u32 = unwrap_ctx!(parse_u32(env!("CARGO_PKG_V
 
 /// Creates a new variable binding.
 #[no_mangle]
-pub extern "C" fn simplexp_new_var(name: *const c_char) -> *const ExprPart {
+pub extern "C" fn simplexp_var_new(name: *const c_char) -> *const ExprPart {
     catch_unwind(|| {
         let name: &str = std::str::from_utf8(unsafe { CStr::from_ptr(name).to_bytes() }).unwrap();
 
         Arc::into_raw(Arc::new(ExprPart::Operation(ExprOp::Var {
             name: name.to_string(),
-        }))) as *const ExprPart
+        })))
     })
     .unwrap_or(null())
 }
 
 /// Wraps the expression(s) in an operation.
 #[no_mangle]
-pub extern "C" fn simplexp_new_op(
+pub extern "C" fn simplexp_op_new(
     op_id: i32,
     child1: *const ExprPart,
     child2: *const ExprPart,
@@ -131,14 +131,14 @@ pub extern "C" fn simplexp_new_op(
             },
         });
 
-        Arc::into_raw(optimizer::optimize(Arc::new(expr))) as *const ExprPart
+        Arc::into_raw(optimizer::optimize(Arc::new(expr)))
     })
     .unwrap_or(null())
 }
 
 /// Creates a new float (f32) literal.
 #[no_mangle]
-pub extern "C" fn simplexp_new_float(value: f32) -> *const ExprPart {
+pub extern "C" fn simplexp_float_new(value: f32) -> *const ExprPart {
     catch_unwind(|| {
         assert!(!value.is_nan());
 
@@ -148,28 +148,27 @@ pub extern "C" fn simplexp_new_float(value: f32) -> *const ExprPart {
             ExprPart::Operation(ExprOp::Inf)
         };
 
-        Arc::into_raw(Arc::new(expr)) as *const ExprPart
+        Arc::into_raw(Arc::new(expr))
     })
     .unwrap_or(null())
 }
 
 /// Creates a new int (i64) literal.
 #[no_mangle]
-pub extern "C" fn simplexp_new_int(value: i64) -> *const ExprPart {
-    catch_unwind(|| Arc::into_raw(Arc::new(ExprPart::IntLiteral(value))) as *const ExprPart)
-        .unwrap_or(null())
+pub extern "C" fn simplexp_int_new(value: i64) -> *const ExprPart {
+    catch_unwind(|| Arc::into_raw(Arc::new(ExprPart::IntLiteral(value)))).unwrap_or(null())
 }
 
 /// Creates a new string literal.
 #[no_mangle]
-pub extern "C" fn simplexp_new_str(value: *const u8, length: usize) -> *const ExprPart {
+pub extern "C" fn simplexp_str_new(value: *const u8, length: usize) -> *const ExprPart {
     catch_unwind(|| {
         if value.is_null() {
             return null();
         }
         let string =
             unsafe { std::str::from_utf8(std::slice::from_raw_parts(value, length)) }.unwrap();
-        Arc::into_raw(Arc::new(ExprPart::StringLiteral(string.to_string()))) as *const ExprPart
+        Arc::into_raw(Arc::new(ExprPart::StringLiteral(string.to_string())))
     })
     .unwrap_or(null())
 }
@@ -184,9 +183,9 @@ pub struct VecInner {
 
 /// Formats an expression into a string.
 #[no_mangle]
-pub extern "C" fn simplexp_format_expr(expr: *const ExprPart) -> VecInner {
+pub extern "C" fn simplexp_expr_format(expr: *const ExprPart) -> VecInner {
     catch_unwind(|| {
-        let expr = unsafe { (expr as *const ExprPart).as_ref().unwrap() };
+        let expr = unsafe { expr.as_ref().unwrap() };
         let (ptr, len, cap) = format!("{:?}", expr).into_bytes().into_raw_parts();
         VecInner {
             ptr: ptr as *const u8,
@@ -204,9 +203,9 @@ pub extern "C" fn simplexp_format_expr(expr: *const ExprPart) -> VecInner {
 /// Serialize an expression into a JSON string.
 /// The format is `{(op), "a": {(op), ...}}`
 #[no_mangle]
-pub extern "C" fn simplexp_serialize_expr(expr: *const ExprPart) -> VecInner {
+pub extern "C" fn simplexp_expr_serialize(expr: *const ExprPart) -> VecInner {
     catch_unwind(|| {
-        let expr = unsafe { (expr as *const ExprPart).as_ref().unwrap() };
+        let expr = unsafe { expr.as_ref().unwrap() };
         let (ptr, len, cap) = serde_json::to_vec(expr).unwrap().into_raw_parts();
         VecInner {
             ptr: ptr as *const u8,
@@ -221,34 +220,35 @@ pub extern "C" fn simplexp_serialize_expr(expr: *const ExprPart) -> VecInner {
     })
 }
 
-// /// Collect operation list, or append to one
-// #[no_mangle]
-// pub extern "C" fn simplexp_make_oplist(expr: *const ExprPart) -> VecInner {
-//     catch_unwind(|| {
-//         let expr = unsafe { (expr as *const ExprPart).as_ref().unwrap() };
-//         let oplist = OperationList::from(expr);
-//         let (ptr, len, cap) = serde_json::to_vec(&oplist).unwrap().into_raw_parts();
-//         VecInner {
-//             ptr: ptr as *const u8,
-//             len,
-//             cap,
-//         }
-//     })
-//     .unwrap_or(VecInner {
-//         ptr: null(),
-//         len: 0,
-//         cap: 0,
-//     })
-// }
+/// Creates a new operation list.
+#[no_mangle]
+pub extern "C" fn simplexp_oplist_new() -> *const () {
+    catch_unwind(|| Arc::into_raw(Arc::new(Mutex::new(OperationList::new()))) as *const ())
+        .unwrap_or(null())
+}
 
-/// Serialize an expression into a JSON string.
+/// Append an expression to an operation list, returning the expression's id in the oplist.
+#[no_mangle]
+pub extern "C" fn simplexp_oplist_append(oplist: *const (), expr: *const ExprPart) -> usize {
+    catch_unwind(|| {
+        let expr = unsafe { expr.as_ref().unwrap() };
+        let oplist = unsafe { (oplist as *const Mutex<OperationList>).as_ref().unwrap() };
+
+        oplist.lock().unwrap().add(expr)
+    })
+    .unwrap_or(usize::MAX)
+}
+
+/// Serialize an oplist into a JSON string.
 /// The format is `[{(op), "a": (ref_id)}, ...]`
 #[no_mangle]
-pub extern "C" fn simplexp_serialize_expr_ops(expr: *const ExprPart) -> VecInner {
+pub extern "C" fn simplexp_oplist_serialize(oplist: *const ()) -> VecInner {
     catch_unwind(|| {
-        let expr = unsafe { (expr as *const ExprPart).as_ref().unwrap() };
-        let oplist = OperationList::from(expr);
-        let (ptr, len, cap) = serde_json::to_vec(&oplist).unwrap().into_raw_parts();
+        let oplist = unsafe { Arc::from_raw(oplist as *const Mutex<OperationList>) };
+        let (ptr, len, cap) = serde_json::to_vec(&*oplist.lock().unwrap())
+            .unwrap()
+            .into_raw_parts();
+        let _ = Arc::into_raw(oplist);
         VecInner {
             ptr: ptr as *const u8,
             len,
@@ -262,9 +262,9 @@ pub extern "C" fn simplexp_serialize_expr_ops(expr: *const ExprPart) -> VecInner
     })
 }
 
-/// Frees a string allocated by `simplexp_format_expr`, `simplexp_serialize_expr`, or `simplexp_serialize_expr_ops`.
+/// Frees a string allocated by `simplexp_expr_format`, `simplexp_expr_serialize`, or `simplexp_oplist_serialize`.
 #[no_mangle]
-pub extern "C" fn simplexp_free_str(inner: VecInner) {
+pub extern "C" fn simplexp_str_free(inner: VecInner) {
     let _ = catch_unwind(|| {
         if inner.ptr.is_null() {
             return;
@@ -273,23 +273,32 @@ pub extern "C" fn simplexp_free_str(inner: VecInner) {
     });
 }
 
-/// Frees an expression allocated by the `simplexp_new_*` functions.
+/// Frees an expression allocated by the `simplexp_{var,int,float,string,op}_new` functions.
 #[no_mangle]
-pub extern "C" fn simplexp_free_expr(expr: *const ExprPart) {
+pub extern "C" fn simplexp_expr_free(expr: *const ExprPart) {
     let _ = catch_unwind(|| {
         assert!(!expr.is_null());
-        let _ = unsafe { Arc::from_raw(expr as *mut ExprPart) };
+        let _ = unsafe { Arc::from_raw(expr) };
     });
 }
 
 /// Creates a copy of the given expression.
 #[no_mangle]
-pub extern "C" fn simplexp_clone_expr(expr: *const ExprPart) -> *const ExprPart {
+pub extern "C" fn simplexp_expr_clone(expr: *const ExprPart) -> *const ExprPart {
     catch_unwind(|| {
         assert!(!expr.is_null());
-        unsafe { Arc::into_raw(Arc::clone_from_ptr(expr)) as *const ExprPart }
+        unsafe { Arc::into_raw(Arc::clone_from_ptr(expr)) }
     })
     .unwrap_or(null())
+}
+
+/// Frees an oplist allocated by the `simplexp_oplist_new` function.
+#[no_mangle]
+pub extern "C" fn simplexp_oplist_free(oplist: *const ()) {
+    let _ = catch_unwind(|| {
+        assert!(!oplist.is_null());
+        let _ = unsafe { Arc::from_raw(oplist as *const Mutex<OperationList>) };
+    });
 }
 
 trait CloneFromPtr<T> {
